42e6dd5c4bf3948176b9e4f9a4efa746c992e7cd
Ryan-hoho <ryanhoho.aus@gmail.com>
2026-04-13 11:21:30 +1000

    IP update

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://15.135.187.115:5001', // live
+  baseURL: 'http://3.25.64.8:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 axiosInstance.interceptors.request.use((config) => {
